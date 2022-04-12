4f9aa079bca9035cd562fc169e65fff672d4e1d7
ulifly <86443463+ulifly@users.noreply.github.com>
2022-04-12 18:59:04 -0500

    working page 2

--- js/ciclo2.js
+++ js/ciclo2.js
@@ -1,8 +1,12 @@
 alert("este es otro ejemplo de ciclo");
 let resp = confirm("esto se repite hasta que canceles");
-do {
-    resp = confirm("esto se repite hasta que canceles");    
-} while (resp = true); {
-    break
+let num = 0;
+    do {
+        num += 1;
+        resp = confirm("esto se repite hasta que canceles");
+    } while (resp == true);   
+if (num <= 3 ) {
+    document.write("solo para que sepas presionaste aceptar " + num + " " + "veces, que poquito probaste!!!!");
+} else {
+    document.write("solo para que sepas presionaste aceptar " + num + " " + "veces, que insistente!!!!!");
 }
-    
