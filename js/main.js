a1cbdc07f3778bddf91877ded97f48d0d308291d
ulifly <86443463+ulifly@users.noreply.github.com>
2022-04-12 17:18:28 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// DESAFÍO COMPLEMENTARIO 1
+
+// Crear un algoritmo utilizando un ciclo
+
+
+
+
+alert("beinvenido \neste es el desafio entregable 1");
+alert("Crear un algoritmo utilizando un ciclo \nvamos a repetir una frase un numero n de veces");
+
+let frase = prompt("ingresa una frase");
+let nveces = parseFloat(prompt("ingresa el numerode veces a repetir"));
+
+for (let i = 1; i <= nveces; i+=1) {
+    innerHTML (frase + i);
+    
+}
+
